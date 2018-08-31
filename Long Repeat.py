70f360c1187b471037a5f7187821c572800edf4f
oleg-tim <book1978@gmail.com>
2018-08-31 16:39:16 +0300

    Update Long Repeat.py

--- Long Repeat.py
+++ Long Repeat.py
@@ -1,3 +1,11 @@
+'''
+Вам необходимо найти длину самой длинной подстроки, которая состоит из одинаковых букв. 
+Например, строка "aaabbcaaaa" состоит из четырех подстрок с одинаковыми буквами "aaa", "bb","c" и "aaaa". 
+Последняя подстрока является самой длинной, что и делает ее ответом.
+
+Входные данные: Строка.
+Выходные данные: Целое число. 
+'''
 def long_repeat(line):
     maxi = 0
     calc = 0
@@ -17,4 +25,3 @@ if __name__ == '__main__':
     assert long_repeat('ddvvrwwwrggg') == 3, "Second"
     assert long_repeat('abababaab') == 2, "Third"
     assert long_repeat('') == 0, "Empty"
-    print('"Run" is good. How is "Check"?')
